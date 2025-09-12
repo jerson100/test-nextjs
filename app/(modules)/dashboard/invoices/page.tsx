515195b56e1ab9ea36f2eaa374a6c7d5bb6dc6ce
Jerson Omar Ramírez Ortiz <jramiror@emeal.nttdata.com>
2025-09-12 00:43:05 -0500

    fix(FONT): changed import

---
+++ app/(modules)/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Invocies Page</div>;
+};
+
+export default page;
---
+++ app/(modules)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Esto es mi dashboard</div>;
+};
+
+export default page;
