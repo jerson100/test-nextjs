d2cc4f220436a087f9856d401f1027ea2243bcba
Jerson Omar Ramírez Ortiz <jramiror@emeal.nttdata.com>
2025-09-14 22:29:22 -0500

    Final course

---
+++ app/(modules)/dashboard/invoices/create/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const loading = () => {
+  return <div>Cargando facturas...</div>;
+};
+
+export default loading;
--- app/(modules)/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Esto es mi dashboard</div>;
-};
-
-export default page;
